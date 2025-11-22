99f930a217752b3baf4bea39bcc14d64849e46d4
Pranav C <pranavxc@gmail.com>
2025-11-22 10:03:53 +0000

    feat: Last modified based on meta column and formula

--- packages/nocodb/src/services/tables.service.ts
+++ packages/nocodb/src/services/tables.service.ts
@@ -41,6 +41,7 @@ import NcConnectionMgrv2 from '~/utils/common/NcConnectionMgrv2';
 import { sanitizeColumnName, validatePayload } from '~/helpers';
 import { MetaTable } from '~/utils/globals';
 import NocoSocket from '~/socket/NocoSocket';
+import { isEE } from '~/utils';
 
 @Injectable()
 export class TablesService {

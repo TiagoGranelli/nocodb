5b4da7e9d7ff6efcd21ab8a069bf55d15f98e59e
Tiago Granelli Ribeiro <tiago.granelli@gmail.com>
2026-02-13 13:35:16 +0100

    removed join cloud cta

--- packages/nc-gui/components/dashboard/Sidebar/UserInfo.vue
+++ packages/nc-gui/components/dashboard/Sidebar/UserInfo.vue
@@ -313,10 +313,6 @@ const copyEmail = () => {
     </div>
 
     <template v-if="!isMiniSidebar">
-      <template v-if="isMobileMode || appInfo.ee"></template>
-      <div v-else class="flex flex-row w-full justify-between pt-0.5 truncate">
-        <GeneralJoinCloud />
-      </div>
     </template>
   </div>
 </template>
